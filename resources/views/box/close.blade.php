1c545fde22e84f8296227fdecafcc28205f7bff9
AreejShaban789 <103071658+AreejShaban789@users.noreply.github.com>
2024-04-23 09:44:09 +0500

    changes in blades

--- resources/views/box/close.blade.php
+++ resources/views/box/close.blade.php
@@ -3,19 +3,21 @@
 @section('content')
 <div class="container mt-6">
   <div class=" d-flex align-items-center justify-content-center" id="giftBox" > 
-{{-- <div class="container text-center mt-0"> --}}
-  {{-- <div class="text-center" id="giftBox" style="margin-top: 0px;"> --}}
-    <img src="{{ asset('assetsfront/img/t4.png') }}" height="55%" width="55%" alt="Closed Gift Box">
+    {{-- <img src="{{ asset('assetsfront/img/t4.png') }}" height="55%" width="55%" alt="Closed Gift Box"> --}}
+    <img src="{{ asset('assetsfront/img/gif2.gif') }}" alt="Open Gift Box" height="55%" width="50%" id="gifImage">
     
   </div>
   <br>
-</div>
+
+  
+
 </div>
 <script>
 document.getElementById('giftBox').addEventListener('click', function() {
-  // Redirect to the open page
   window.location.href = "{{ route('box.open') }}";
 });
+
+
 </script>
 
 @endsection
--- resources/views/box/open.blade.php
+++ resources/views/box/open.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
 <div class="container mt-6 ">
-    <br><br>
-    <br>
+    
     <div class="text-center d-flex align-items-center justify-content-center" id="giftBox" > 
-        <br><br>
-{{-- <div class="container text-center mt-0 "> --}}
-  {{-- <div class="text-center" id="giftBox" style="margin-top: 0px;"> --}}
+      
     <img src="{{ asset('assetsfront/img/gif1.gif') }}" alt="Open Gift Box" height="55%" width="50%" id="gifImage">
     
   </div>
